003451dc528f955b358aa4b00bf892361199c5ee
Florian Atteneder <florian.atteneder@gmail.com>
2022-03-27 20:32:27 +0200

    Implement Presentation struct and multi-paged pdf output

---
+++ examples/mwe.jl
@@ -0,0 +1,33 @@
+using MakieSlides
+
+
+thisdir = basename(@__DIR__)
+hide_decorations = true
+
+slide1 = Slide(hide_decorations=hide_decorations)
+slideheader!(slide1, "My first slide")
+slidetext!(slide1, """
+hello world! Here follows a very long text which I don't know about
+whether it will line break or not... probably not
+
+- blabla
+- mimimi
+- uga-aga-uga-aga
+""")
+slidefooter!(slide1, "Florian Atteneder")
+@time MakieSlides.save(joinpath(thisdir, "slide1.pdf"), slide1)
+
+
+slide2 = Slide(hide_decorations=hide_decorations)
+slideheader!(slide2, "My first slide")
+slidetext!(slide2, """
+# Is this really page 2?
+""")
+slidefooter!(slide2, "Florian Atteneder")
+
+@time MakieSlides.save(joinpath(thisdir, "slide2.pdf"), slide2)
+
+
+presentation = Presentation()
+append!(presentation.slides, [ slide1, slide2 ])
+@time MakieSlides.save(joinpath(thisdir, "presentation.pdf"), presentation)
--- src/MakieSlides.jl
+++ src/MakieSlides.jl
@@ -3,27 +3,20 @@ module MakieSlides
 
 using GLMakie
 using CairoMakie
+import Cairo
 
 
 export Slide,
        slidetext!,
        slideheader!,
        slidefooter!,
+       Presentation,
        save
-
-#=
-mutable sturct Presentation
-  slides::Vector{Slides}
-  title::String
-  author::String
-  slidenumbers::Bool
-end
-=#
 
 
 struct Slide
-  fig
-  axis::Dict
+  figure
+  panes::Dict
 end
 
 
@@ -33,76 +26,113 @@ function Slide(; hide_decorations::Bool=true,
 
   @assert all(aspect .> 0)
 
-  fig = Figure()
+  figure = Figure()
 
-  axis = Dict(:header  => Axis(fig[1,1]),
-              :content => Axis(fig[2,1]),
-              :footer  => Axis(fig[3,1]))
+  panes = Dict(:header  => Axis(figure[1,1]),
+               :content => Axis(figure[2,1]),
+               :footer  => Axis(figure[3,1]))
 
   ratio_header = 1/10
-  rowsize!(fig.layout, 1, Auto(ratio_header))
+  rowsize!(figure.layout, 1, Auto(ratio_header))
   ratio_footer = 1/10
-  rowsize!(fig.layout, 3, Auto(ratio_footer))
+  rowsize!(figure.layout, 3, Auto(ratio_footer))
 
   aspects = Dict(:header => (aspect[1], 1+aspect[2]*ratio_header),
                  :content => aspect,
                  :footer => (aspect[1], 1+aspect[2]*ratio_footer))
 
-  for (k, ax) in pairs(axis)
+  for (k, pane) in pairs(panes)
     asp = aspects[k]
-    limits!(ax, 1, asp[1], 1, asp[2])
+    limits!(pane, 1, asp[1], 1, asp[2])
     if hide_decorations
-      hidedecorations!(ax,grid=true)
-      hidespines!(ax)
+      hidedecorations!(pane, grid=true)
+      hidespines!(pane)
     end
   end
 
-  rowgap!(fig.layout, 5)
-  colgap!(fig.layout, 5)
+  rowgap!(figure.layout, 5)
+  colgap!(figure.layout, 5)
 
-  return Slide(fig, axis)
+  return Slide(figure, panes)
 end
 
 
 function slidetext!(slide, text)
-  pane = slide.axis[:content]
+  pane = slide.panes[:content]
   rect = pane.targetlimits[]
   xwidth, ywidth = rect.widths
   xorigin, yorigin = rect.origin
   origin_text = Point2f(xorigin,yorigin+ywidth)
   t = text!(pane, text, position=origin_text, align=(:left,:top))
-  display(t.attributes)
 end
 
 
 function slideheader!(slide, text)
-  pane = slide.axis[:header]
+  pane = slide.panes[:header]
   rect = pane.targetlimits[]
   xwidth, ywidth = rect.widths
   xorigin, yorigin = rect.origin
   origin_text = Point2f(xorigin,yorigin+ywidth/2)
   t = text!(pane, text, position=origin_text, align=(:left,:center))
-  display(t.attributes)
 end
 
 
 function slidefooter!(slide, text)
-  pane = slide.axis[:footer]
+  pane = slide.panes[:footer]
   rect = pane.targetlimits[]
   xwidth, ywidth = rect.widths
   xorigin, yorigin = rect.origin
   origin_text = Point2f(xorigin,yorigin+ywidth/2)
   t = text!(pane, text, position=origin_text, align=(:left,:center))
-  display(t.attributes)
 end
 
 
-Base.display(s::Slide) = display(s.fig)
+Base.display(s::Slide) = display(s.figure)
 
 
 function save(name, s::Slide)
   CairoMakie.activate!()
-  CairoMakie.save(name, s.fig)
+
+  scene = s.figure.scene
+  screen = CairoMakie.CairoScreen(scene, name, :pdf)
+  CairoMakie.cairo_draw(screen, scene)
+  Cairo.finish(screen.surface)
+
+  GLMakie.activate!()
+end
+
+
+Base.@kwdef mutable struct Presentation
+  slides::Vector{Slide} = Slide[]
+  title::String = ""
+  author::String = ""
+  slidenumbers::Bool = false
+end
+
+
+# function Base.display(presentation::Presentation)
+#   #display(s.figure)
+# end
+
+
+function save(name, presentation::Presentation)
+  CairoMakie.activate!()
+
+  slides = presentation.slides
+  @assert length(slides) > 0
+
+  figure = Figure()
+  scene = figure.scene
+  screen = CairoMakie.CairoScreen(scene, name, :pdf)
+
+  for (idx, slide) in enumerate(slides)
+    CairoMakie.cairo_draw(screen, slide.figure.scene)
+    idx == length(slides) && break
+    Cairo.show_page(screen.context)
+    Cairo.restore(screen.context)
+  end
+  Cairo.finish(screen.surface)
+
   GLMakie.activate!()
 end
 
